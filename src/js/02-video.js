9c7b7ea216b760a71519a594005236f3a12a343f
onepopina <anyarasp@gmail.com>
2023-02-19 21:00:26 +0100

    task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,16 +6,10 @@ const player = new Player(iframe);
 const STORAGE_TIME_KEY = 'videoplayer-current-time';
 
 function saveTime({ seconds }) {
-  try {
-    localStorage.setItem(STORAGE_TIME_KEY, seconds);
-  } catch (err) {
-    console.error(err);
-  }
+  localStorage.setItem(STORAGE_TIME_KEY, seconds);
 }
-
 const throttleSaveTime = throttle(saveTime, 1000);
+player.on('timeupdate', throttleSaveTime);
 
 const seconds = Number(localStorage.getItem(STORAGE_TIME_KEY) || 0);
 player.setCurrentTime(seconds);
-
-player.on('timeupdate', throttleSaveTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,3 +7,36 @@ const refs = {
 };
 
 const STORAGE_INPUT_KEY = 'feedback-form-state';
+
+function getFormData() {
+  return { email: refs.email.value, message: refs.message.value };
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(getFormData());
+  refs.email.value = '';
+  refs.message.value = '';
+  localStorage.removeItem(STORAGE_INPUT_KEY);
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onInput() {
+  localStorage.setItem(STORAGE_INPUT_KEY, JSON.stringify(getFormData()));
+}
+
+const updateStorage = throttle(onInput, 500);
+
+refs.form.addEventListener('input', updateStorage);
+
+function innitData() {
+  const formData = localStorage.getItem(STORAGE_INPUT_KEY);
+  if (formData) {
+    const { email, message } = JSON.parse(formData);
+    refs.email.value = email;
+    refs.message.value = message;
+  }
+}
+
+innitData();
