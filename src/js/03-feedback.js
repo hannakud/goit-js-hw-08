ace1e9f5622c10f22deced719719210e4280d5d7
hannakud <104619011+hannakud@users.noreply.github.com>
2023-02-19 19:04:27 +0000

    Deploying to gh-pages from @ hannakud/goit-js-hw-08@22087ecc36ebabba56dbb0475fdbaecd4003724a 🚀

--- src/js/03-feedback.js
+++
@@ -1,9 +0,0 @@
-import throttle from 'lodash.throttle';
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
-};
-
-const STORAGE_INPUT_KEY = 'feedback-form-state';
